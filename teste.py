87c53a06bf673c542f1db8e074a1de285494595b
settifoglio <froma.galuppo@gmail.com>
2017-08-23 19:25:25 -0300

    Distribuição normal é easy

--- teste.py
+++ teste.py
@@ -1,6 +1,16 @@
+#%%
+import numpy as np
 import functions as f
 import networkx as nx
 
+#%%
 g = nx.read_edgelist("email-Eu-core.txt", nodetype=int)
 
 f.plot(g)
+
+#%%
+print("Elementar")
+
+#%%
+s = np.random.normal(0, 1, g.number_of_nodes())
+print(s)
