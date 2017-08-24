e64b707feb4dfe6058a14340628d1e9a1b30475f
settifoglio <froma.galuppo@gmail.com>
2017-08-23 23:44:49 -0300

    Bruno, é isso?

--- ITC.py
+++ ITC.py
@@ -1,1 +1,35 @@
 # Método mais simples
+
+# Módulos
+import numpy as np
+import networkx as nx
+
+# Função a
+def a(valor):
+	if valor <= 0: return 0
+	return 1
+
+# Entradas
+arq = input("Arquivo:\n> ")
+alpha = float(input("\nAlpha:\n> "))
+ite = int(input("\nIterações:\n> "))
+
+# Declarações
+results = []
+g = nx.read_edgelist(arq, nodetype=int)
+N = g.number_of_nodes()
+
+# Laço
+for j in range(ite):
+	out = 0
+
+	# Componente Estocástico
+	U = np.random.normal(0, 1, N)
+
+	# ((Somatório do tratamento 1) - (Somatório do tratamento 0)) / N
+	for i in range(N):
+		out += a(U[i] + alpha) - a(U[i])
+	results.append(out / N)
+
+print("\nMédia: {}\nVariância: {}".format(np.median(results), np.var(results)))	
+	
