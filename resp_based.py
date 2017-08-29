b33e675d72276cbe85e67c9737895f3b4c639d0c
settifoglio <froma.galuppo@gmail.com>
2017-08-28 23:11:52 -0300

    Agora imprime a média

--- resp_based.py
+++ resp_based.py
@@ -21,9 +21,11 @@ g = nx.read_edgelist(arq, nodetype=int)
 N = g.number_of_nodes()
 A = nx.to_numpy_matrix(g)
 
+# Vetores com Tratamento = 1 e com Tratamento = 0
 Y1 = np.zeros(N)
 Y0 = np.zeros(N)
 
+# Tempo discreto
 for i in range(ite):
 	X1 = Y1
 	U1 = np.random.normal(0, 1, N)
@@ -31,6 +33,10 @@ for i in range(ite):
 	X0 = Y0
 	U2 = np.random.normal(0, 1, N)
 
+	# Aplica o Tratamento 1 e 0 para todo nó
 	for j in range(N):
 		Y1[j] = a(alpha + beta + (gama * np.asscalar(np.dot(X1, A[j].transpose())) / N) + U1[j])
 		Y0[j] = a(alpha + (gama * np.asscalar(np.dot(X0, A[j].transpose())) / N) + U2[j])
+
+# Imprime a média entre os tratamentos
+print("Média:", (sum(Y1) - sum(Y0)) / N)
