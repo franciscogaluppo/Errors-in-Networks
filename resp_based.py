966045f384514de046ed9053a604723cf6c1ee67
settifoglio <froma.galuppo@gmail.com>
2017-09-01 10:16:05 -0300

    Consertado

--- resp_based.py
+++ resp_based.py
@@ -1,42 +1,87 @@
-# Método mais robusto
-
 # Módulos
+from random import random as rd
 import numpy as np
 import networkx as nx
+
 
 # Função a
 def a(valor):
 	if valor <= 0: return 0
 	return 1
 
+
 # Entradas
 arq = input("Arquivo: ")
 alpha = float(input("Alpha: "))
 beta = float(input("Beta: "))
 gama = float(input("Gama: "))
-ite = int(input("Iterações: "))
+cent = int(input("%z=0: "))
+T = int(input("T: "))
+
 
 # Declarações
 g = nx.read_edgelist(arq, nodetype=int)
 N = g.number_of_nodes()
 A = nx.to_numpy_matrix(g)
 
-# Vetores com Tratamento = 1 e com Tratamento = 0
-Y1 = np.zeros(N)
-Y0 = np.zeros(N)
+
+# cent% da população recebe o tratamento z=0 e o restante o z=1
+# A resposta inicial de ambos é y=0
+for i in range(N):
+	if rd() < cent/100:
+		g.node[i]['y'] = 0
+		g.node[i]['z'] = 0
+	else:
+		g.node[i]['y'] = 0
+		g.node[i]['z'] = 1
+
+
+# Vetor auxiliar para armazenar os valores atuais
+Y = np.zeros(N)
 
 # Tempo discreto
-for i in range(ite):
-	X1 = Y1
-	U1 = np.random.normal(0, 1, N)
+for i in range(T):
 
-	X0 = Y0
-	U2 = np.random.normal(0, 1, N)
+	# Componente estocástico
+	U = np.random.normal(0, 1, N)
 
 	# Aplica o Tratamento 1 e 0 para todo nó
 	for j in range(N):
-		Y1[j] = a(alpha + beta + (gama * np.asscalar(np.dot(X1, A[j].transpose())) / g.degree(j)) + U1[j])
-		Y0[j] = a(alpha + (gama * np.asscalar(np.dot(X0, A[j].transpose())) / g.degree(j)) + U2[j])
+		
+		# Soma dos tratamentos dos nós vizinhos de j
+		soma = 0
+		for k in g.neighbors(j): soma += g.node[k]['y']
 
-# Imprime a média entre os tratamentos
-print("Média:", (sum(Y1) - sum(Y0)) / N)
+		# Aplica a função ao nó j
+		Y[j] = a(alpha + (beta * g.node[j]['z']) + (gama * soma/ g.degree(j)) + U[j])
+
+	# Atualiza as respostas dos nós com os novos valores
+	for j in range(N):
+		g.node[j]['y'] = Y[j]
+
+
+# Resultados
+y1 = 0
+y1z1 = 0
+
+
+# Contagem
+for i in range(N):
+	if g.node[i]['y'] == 1:
+		y1 += 1
+		if g.node[i]['z'] == 1:
+			y1z1 += 1
+
+
+# Imprime os resultados
+print("\nFração de nós com Yi=1: {}".format(y1/N))
+
+if N - y1 != 0:
+	print("Fração de nós com Yi=1 dado que Z=0: {}".format((y1 - y1z1) / (N - y1)))
+else:
+	print("Não há nós com Yi=0")
+
+if y1 != 0:
+	print("Fração de nós com Yi=1 dado que Z=1: {}".format(y1z1/y1))
+else:
+	print("Não há nós com Yi=1")
