2e5650ad0a38eb5e22ead680d769805b351fdb2f
settifoglio <froma.galuppo@gmail.com>
2017-09-01 12:50:12 -0300

    Seleciona o tipo da simulação

--- main.py
+++ main.py
@@ -3,34 +3,39 @@ import numero
 import fracao
 import resp_based
 
+sim = int(input("[1]ITR\n[2]Número\n[3]Fração\n[4]Response Based\n\n> "))
+print("")
 
-# Fiquei na dúvida se as entradas são análogas ou não
+if sim == 1:
+	arq = input("Arquivo: ")
+	alpha = float(input("Alpha: "))
+	cent = int(input("%z=0: "))
+	ite = int(input("Ite: "))
+	ITR.itr(arq, alpha, ite, cent)
 
-# Número (menos o kappa)
-# arq = raw_input("Arquivo: ")
-# alpha = float(input("Alpha: "))
-# beta = float(input("Beta: "))
-# gama = float(input("Gama: "))
+elif sim == 2:
+	arq = input("Arquivo: ")
+	alpha = float(input("Alpha: "))
+	beta = float(input("Beta: "))
+	gama = float(input("Gama: "))
+	kappa = float(input("Kappa: "))
+	cent = int(input("%z=0: "))
+	numero.init(arq, alpha, beta, gama, kappa, cent)
 
-# Fração (menos o tau)
-# arq = input("Arquivo: ")
-# alpha = float(input("Alpha: "))
-# beta = float(input("Beta: "))
-# gama = float(input("Gama: "))
-# porcentagem = float(input("Porcentagem: "))
+elif sim == 3:
+	arq = input("Arquivo: ")
+	alpha = float(input("Alpha: "))
+	beta = float(input("Beta: "))
+	gama = float(input("Gama: "))
+	tau = float(input("Tau: "))
+	cent = float(input("%z=0: "))
+	fracao.init(arq, alpha, beta, gama, tau, cent)
 
-# Entradas - Como vão ficar?
-arq = input("Arquivo: ")
-alpha = float(input("Alpha: "))
-beta = float(input("Beta: "))
-gama = float(input("Gama: "))
-kappa = float(input("Kappa: "))
-tau = float(input("Tau: "))
-cent = int(input("%z=0: "))
-ite = int(input("Ite: "))
-T = int(input("T: "))
-
-ITR.itr(arq, beta, ite, cent)
-numero.init(arq, alpha, beta, gama, kappa, cent)
-fracao.init(arq, alpha, beta, gama, tau, cent)
-resp_based.resp_based(arq, alpha, beta, gama, cent, T)
+elif sim == 4:
+	arq = input("Arquivo: ")
+	alpha = float(input("Alpha: "))
+	beta = float(input("Beta: "))
+	gama = float(input("Gama: "))
+	cent = int(input("%z=0: "))
+	T = int(input("T: "))
+	resp_based.resp_based(arq, alpha, beta, gama, cent, T)
