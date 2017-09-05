4837ec9830f4e4512124b8767e76e86ce6edf6d7
settifoglio <froma.galuppo@gmail.com>
2017-09-05 17:36:13 -0300

    Agora usa as funções

--- main.py
+++ main.py
@@ -1,41 +1,29 @@
-import ITR
-import numero
-import fracao
-import resp_based
+# Importa os modelos
+from ITR import itr
+from numero import init as num
+from fracao import init as frac
+from resp_based import resp_based as resp
 
+# Importa as funções
+from funcs import print_out as po
+from funcs import get_input as gt
+
+# Seleciona o modelo
 sim = int(input("[1]ITR\n[2]Número\n[3]Fração\n[4]Response Based\n\n> "))
 print("")
 
+# Recebe as entradas
+ins = gt(sim)
+
+# Chama o modelo
 if sim == 1:
-	arq = input("Arquivo: ")
-	alpha = float(input("Alpha: "))
-	cent = int(input("%z=0: "))
-	ite = int(input("Ite: "))
-	ITR.itr(arq, alpha, ite, cent)
+	po(sim, itr(ins))
 
 elif sim == 2:
-	arq = input("Arquivo: ")
-	alpha = float(input("Alpha: "))
-	beta = float(input("Beta: "))
-	gama = float(input("Gama: "))
-	kappa = float(input("Kappa: "))
-	cent = int(input("%z=0: "))
-	numero.init(arq, alpha, beta, gama, kappa, cent)
+	po(sim, num(ins))
 
 elif sim == 3:
-	arq = input("Arquivo: ")
-	alpha = float(input("Alpha: "))
-	beta = float(input("Beta: "))
-	gama = float(input("Gama: "))
-	tau = float(input("Tau: "))
-	cent = float(input("%z=0: "))
-	fracao.init(arq, alpha, beta, gama, tau, cent)
+	po(sim, frac(ins))
 
 elif sim == 4:
-	arq = input("Arquivo: ")
-	alpha = float(input("Alpha: "))
-	beta = float(input("Beta: "))
-	gama = float(input("Gama: "))
-	cent = int(input("%z=0: "))
-	T = int(input("T: "))
-	resp_based.resp_based(arq, alpha, beta, gama, cent, T)
+	po(sim, resp(ins))
