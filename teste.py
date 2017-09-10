492bb8476dd2d42ceed9a6124cb1e81e9940e862
settifoglio <froma.galuppo@gmail.com>
2017-09-10 18:57:12 -0300

    Arquivos testes

---
+++ teste.py
@@ -0,0 +1,9 @@
+# Teste da entrada por arquivo
+
+from resp_based import resp_based as resp
+from funcs import get_input as gt
+from funcs import print_out as po
+
+ins = gt(4)
+
+po(4, resp(ins, "zeta.txt"))
