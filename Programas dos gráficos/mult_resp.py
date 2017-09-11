2bd97ccd008c76ba671096405b178397662709fe
settifoglio <froma.galuppo@gmail.com>
2017-09-11 00:55:34 -0300

    Testes

--- Programas dos gráficos/mult_resp.py
+++ Programas dos gráficos/mult_resp.py
@@ -1,27 +1,25 @@
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-from Modelos.numero import num
+from Modelos.resp_based import resp
 
 alpha = np.sort(np.random.normal(0, 1, 10))
 beta = np.sort(np.random.normal(0, 1, 10))
 gama = np.sort(np.random.normal(0, 1, 10))
-kappa = np.sort(np.random.normal(10, 4, 10))
 
 Y = []
 
 for a in alpha:
 	for b in beta:
 		for g in gama:
-			for k in kappa:
-				Y.append(num(["set1.txt", a, b, g, k], "zeta.txt")[0])
+			Y.append(resp(["set1.txt", a, b, g, 10], "zeta.txt")[0])
 
 fig = plt.figure()
 ax = fig.gca()
 
-ax.set_xticks(np.arange(0, 10000, 1000))
+ax.set_xticks(np.arange(0, 1000, 100))
 ax.set_yticks(np.arange(0, 1., 0.1))
 
-plt.scatter(np.arange(0, 10000, 1), Y)
+plt.scatter(np.arange(0, 1000, 1), Y)
 plt.grid()
 plt.show()
