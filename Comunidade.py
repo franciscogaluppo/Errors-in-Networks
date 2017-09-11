bc212eb71bf74b3dd56cc77debe749fe13a2cd57
brunodemattos <31261246+brunodemattos@users.noreply.github.com>
2017-09-11 18:57:27 -0300

    Add files via upload

---
+++ Comunidade.py
@@ -0,0 +1,15 @@
+f = open("com.txt", "r")
+
+maior = 0						#variaveis para armazenar o numero de elemntos da maior string e essa string
+string = "1"
+
+for line in f:                  #descobre a linha com maior numero de elementos e armazena em lista
+	if line.count("	") > maior:
+		string = line
+		maior = line.count("	")
+
+string = string[:-1]            #remove o \n do final da string
+lista = string.split("	")      #tranforma a string numa lista de strings
+
+for i in range(len(lista)):     #tranforma as strings em ints
+	lista[i] = int(lista[i])
