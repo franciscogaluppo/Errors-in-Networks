Fix calculation of fraction of treated neighbors

Modelos/fracao.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import numpy as np
-
 from comu import comunidade as com
+#import csv
 
 # Função a
 def a(valor):
@@ -42,22 +42,18 @@ def frac(g, ins, zvector, U, comu=None):
 
 	#filename = 'a{0}_b{1}_g{2}.txt'.format(alpha, beta, gama)
 	#with open(filename,'w') as csvfile:
-	    #datafile = csv.writer(csvfile)
+	#	datafile = csv.writer(csvfile)
 	for i in range(N):
-
-		soma = 0.0
+		soma = np.float64(0.0)
 		for k in g.neighbors(i): soma += g.node[k]['z']
-		frac = soma / g.degree(i)
-
+		#TODO: decide for isolated nodes if fraction should be 0 or 1
+		frac = soma / g.degree(i) if g.degree(i) else 1
 		if g.node[i]['z'] == 0 and frac < tau and not linear:
 			g.node[i]['y'] = a(alpha + U[i])
-
 		elif g.node[i]['z'] == 1 and frac > tau and not linear:
 			g.node[i]['y'] = a(alpha + beta + U[i])
-
 		elif binario:
 			g.node[i]['y'] = a(alpha + beta*g.node[i]['z'] + gama*frac + U[i])
-
 		else:
 			g.node[i]['y'] = alpha + beta*g.node[i]['z'] + gama*frac + U[i]
 			#datafile.writerow([g.node[i]['z'],frac,g.node[i]['y']])

funcs.py
@@ -8,6 +8,8 @@ import networkx as nx
 from sklearn import linear_model
 from os import listdir
 import ipdb
+import numpy as np
+import sys
 
 from Modelos.ITR import itr
 from Modelos.numero import num
@@ -137,6 +139,9 @@ def count_y1z0(vecz, vecy):
 # Estima o valor do ATE
 def ate_estimate(zvec, yvec, name, est_model):
 	g = nx.read_edgelist(path(name), nodetype=int)
+        selfloops = [ (i,i) for i in g.nodes_with_selfloops()]
+        g.remove_edges_from(selfloops)
+
 	N = len(zvec)
 
 	# SUTVA
@@ -173,7 +178,10 @@ def ate_estimate(zvec, yvec, name, est_model):
 		soma = 0.0
 		for k in g.neighbors(i):
 			soma += zvec[k]
-		tau.append(soma/g.degree(i))
+                if g.degree(i) == 0:
+                    tau.append(1.0)
+                else:
+                    tau.append(soma/g.degree(i))
 
 	# Vetor das features
 	features = []
@@ -336,6 +344,8 @@ def yfile_to_yvector(name, yvec_run, modelo, ins_run, zvec_run):
 # Simula um dos modelos
 def simulate(model, zvec, ins, name, U=None):
 	g = nx.read_edgelist(path(name), nodetype=int)
+        selfloops = [ (i,i) for i in g.nodes_with_selfloops()]
+        g.remove_edges_from(selfloops)
 
 	if model == 1:
 		return(itr(g, ins, zvec))
@@ -344,7 +354,7 @@ def simulate(model, zvec, ins, name, U=None):
 		return(num(g, ins, zvec))
 
 	elif model == 3:
-		return(frac(g, ins, zvec, U))
+		return(np.array(frac(g, ins, zvec, U)))
 
 	elif model == 4:
 		return(resp(g, ins, zvec))
@@ -355,4 +365,4 @@ def real_ATE(model, ins, name):
 	g = nx.read_edgelist(path(name), nodetype=int)
 	N = g.number_of_nodes()
         U = [0]*N
-	return((sum(simulate(model, cent(100, N), ins, name, U)) - sum(simulate(model, cent(0, N), ins, name, U)))/N)
+	return((sum(simulate(model, [1]*N, ins, name, U) - simulate(model, [0]*N, ins, name, U)))/N)

plots.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from os import listdir
 import funcs as f
+import numpy as np
 
 
 # FUNÇÕES PARA A ALTURA
@@ -19,12 +20,16 @@ def distance(a, b):
 # Calcula a distância real entre o ATE e o valor estimado pela regressão linear
 def real_dist(nome, model, ins, zvec, RUNS=100):
 	real = f.real_ATE(model, ins, nome)
+        #100FIXME: remove line below
+        real2 = ins[1]+ins[2]
+        print({'ATE real':real, 'beta+gamma':real2})
 	for run in xrange(RUNS):
 		esti = f.ate_estimate(zvec, f.simulate(model, zvec, ins, nome), nome, 2)
 		if run == 0:
 			sum_esti = esti
 		else:
 			sum_esti += esti
+        print([real,sum_esti/RUNS])
 	return(distance(real, sum_esti/RUNS))
 
 # Calcula a distância real com a função a() entre o ATE e o valor estimado pela regressão probit
@@ -91,13 +96,13 @@ if model == 4:
 #  -- SIMULAÇÕES
 
 # Roda a simulação para diferentes gammas
-for gamma in [x / 10.0 for x in range(1, 11, 1)]:
+for gamma in [x / np.float64(10.0) for x in range(1, 11, 1)]:
 
 	# Novo valor de gamma
 	ins[2] = gamma
 
 	# E para diferentes betas
-	for beta in [x / 10.0 for x in range(1, 11, 1)]:
+	for beta in [x / np.float64(10.0) for x in range(1, 11, 1)]:
 
 		# Novo valor de beta
 		ins[1] = beta