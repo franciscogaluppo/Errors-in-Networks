b8a2b81b275d030f6e04cb07f9d1962fd3e43410
settifoglio <froma.galuppo@gmail.com>
2017-11-22 09:55:14 -0200

    Cria um arquivo com o tratamento e vetor de tratamentos

---
+++ ZF_file.py
@@ -0,0 +1,17 @@
+from sys import argv
+from funcs import get_graph
+from funcs import ZF_file as zf
+from funcs import zfile_to_zvector as ztz
+
+# Entrada
+nome = argv[1]
+zvec_run = int(argv[2])
+
+# Acha o grafo e seu tratamento
+g = get_graph(nome)
+zvector = ztz(nome, zvec_run)
+
+# Cria a saida
+nome = nome + "_VETORES_TRATAMENTO_&_FRAC.txt"
+zf(g, zvector, nome)
+print("Pronto!")
