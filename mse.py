d9781134f504920c2ee694063e538e097fa42099
settifoglio <froma.galuppo@gmail.com>
2017-11-27 12:58:59 -0200

    Testes de convergências do MSE: teórico x experimental

--- mse.py
+++ mse.py
@@ -1,13 +1,14 @@
 import funcs as f
 import numpy as np
+from sys import argv
 
-nome = "email-Eu-core"	  # Nome do Dataset
+nome = "p2p-Gnutella08"	  # Nome do Dataset
 g = f.get_graph(nome)     # Grafo
-rodadas = 1000            # Número de rodadas
+rodadas = int(argv[1])    # Número de rodadas
 trat = 1		  	      # Número do vetor de tratamento
 model = 3		          # Número do modelo da simulação
 beta = [0, 1, 1]          # Vetor Beta
-ins = [True, 0, False]    # Linear (True) e não Binário (False)
+ins = [True, 0, True]     # Linear (True) e não Binário (False)
 
 N = g.number_of_nodes()
 zvec = f.zfile_to_zvector(nome, trat)
@@ -15,7 +16,7 @@ zvec = f.zfile_to_zvector(nome, trat)
 media = 0                 # Média da distribuição Normal
 var = 1                   # Variância da distribuição Normal
 
-est_model = 2
+est_model = 4
 predicoes = np.empty(rodadas)
 ATE = np.empty(rodadas)
 
@@ -25,6 +26,8 @@ for i in range(rodadas):
 	yvec = f.simulate(g, model, zvec, beta, ins, U)
 	predicoes[i] = f.ate_estimate(g, zvec, yvec, est_model)
 	ATE[i] = f.real_ATE(g, model, beta, ins, U)
+
+	print("Rodada {} de {}".format(i+1, rodadas))
 
 mse = ((predicoes - ATE) ** 2).mean()
 print(mse)
