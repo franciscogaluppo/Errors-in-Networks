b28ba897eee801fad9752099abe8777e88798dd6
Bruno <brunodemattos1998@gmail.com>
2017-12-05 13:02:13 -0200

    Atualização do FENNEL e do LDG

--- FENNEL.cpp
+++ FENNEL.cpp
@@ -3,87 +3,123 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	//tamanho do vetor
 	int n = atoi(argv[1]);
 	vector <vector<int>> m (n);
 
-	FILE *f = fopen("grafo.txt", "r");
+	//Cria a lista de adjacencia
+	FILE *f = fopen("Datasets/SlashDot/set.txt", "r");
+	if (f == NULL){
+		printf("Arquivo set nao encontrado\n");
+		return 1;
+	}
 	int a, b;
 	while (fscanf(f, 	"%i %i", &a, &b) != EOF){
 		m[a].push_back(b);
+		//se comentar a linha de baixo o grafo passa a ser dirigido
 		m[b].push_back(a);
 	}
+	fclose(f);
 
+	//numero de clusters
 	int clusters = atoi(argv[2]);
 
-	//Vetor que vai guardar a que comunidade pertence cada vertice
-	int centro[n];
-	int tamanho[clusters];
+	//vetor que vai guardar a que comunidade pertence cada vertice
+	vector <int> centro (n, -1);
+	//vetor que guarda o tamanho de cada cluster
+	vector <int> tamanho (clusters, 0);
 
-	double alpha = stod(argv[3]);
-
-	clock_t t = clock();
-
-	//coloca os 'clusters' primeiros vertices como centro de suas comunidades e o tamanho de cada grupo como 1
-	for(int i = 0; i < clusters*clusters; i++){
-		tamanho[i % clusters] = clusters;
-		centro[i] = i % clusters;
+	//define as comunidades dos primeiros vertices
+	for(int i = 0; i < clusters; i++){
+		centro[rand() % n] = i;
+		tamanho[i] += 1;
 	}
 
+	//capacidade maxima de cada cluster
+	double limite = (double) n / clusters;
+	//o tanto que pode variar o tamanho
+	//limite *= 1.1;
 
-	for(int i = clusters*clusters; i < n; i++){
-		double coeficiente[clusters];
-		for(int j = 0; j < clusters; j++)
-			coeficiente[j] = 0;
 
+	int iteracoes = atoi(argv[3]);
+	double alpha = atof(argv[4]);
+	for(int k = 0; k < iteracoes; k++){
 
-		//itera pelos vizinhos e soma ao coeficiente de cada grupo o numero de vizinhos
-		for (int j = 0; j < m[i].size(); j++){
-			if (m[i][j] > i)
+		//quais vertices ja foram visitados nessa iteracao
+		vector <int> vis (n, 0);
+
+		queue <int> fila;
+		fila.push(rand() % n);
+
+		for (int l = 0; l < n; l++){
+			//se ja foi visitado continua
+			if (vis[l])
 				continue;
-			int indice = centro[m[i][j]];
-			coeficiente[indice] += 1;
+
+			queue <int> fila;
+			fila.push(l);
+
+			//bfs
+			while(fila.size()){
+				int u = fila.front();
+				fila.pop();
+
+				if(vis[u])
+					continue;
+
+				//marca como visitado
+				vis[u] = 1;
+				//adiciona os vizinhos na fila
+				for(int i = 0; i < m[u].size(); i++)
+					if(!vis[m[u][i]])
+						fila.push(m[u][i]);
+
+				//se, na primeira iteracao, ja tiver comunidade, pula
+				if(!k && centro[u] > -1)
+					continue;
+
+				vector <double> coeficiente (clusters, 0);
+
+				for(int i = 0; i < m[u].size(); i++){
+					int indice = centro[m[u][i]];
+					if (indice > -1)
+						coeficiente[indice] += 1;
+				}
+
+				for(int i = 0; i < clusters; i++)
+					coeficiente[i] -= alpha*tamanho[i];
+
+				//descobre qual e o cluster com maior coeficiente
+				int maior = 0;
+				for(int j = 1; j < clusters; j++){
+					if (coeficiente[j] > coeficiente[maior])
+						maior = j;
+					//o criterio de desempate e o tamanho
+					else if (coeficiente[j] == coeficiente[maior])
+						maior = tamanho[maior] > tamanho[j]? j : maior;
+					}
+
+				centro[u] = maior;
+				tamanho[maior] += 1;
+			}
 		}
 
-
-		//calcula o coeficiente de cada cluster
-		for(int j = 0; j < clusters; j++)
-			coeficiente[j] -= alpha*tamanho[j];
-
-
-		//descobre a comunidade que tem o maior coeficiente
-		int maior = 0;
-		for(int j = 1; j < clusters; j++)
-			if (coeficiente[j] > coeficiente[maior])
-				maior = j;
-
-		// printf("comunidade escolhida: %i\n", maior);
-
-		//O vertice i sera colocado na comunidade maior
-		centro[i] = maior;
-		tamanho[maior] += 1;
-
-		// if(i % 1000 == 0)
-		// 	printf("%i\n", i);
-
-
+		if (k < iteracoes - 1)
+			for(int i = 0; i < clusters; i++)
+				tamanho[i] = 0;
 	}
-
-	t = clock() - t;
-	double tempo = (double) t / CLOCKS_PER_SEC;
 
 	for(int i = 0; i < clusters; i++)
 		printf("O tamanho do cluster %i e %i\n", i + 1, tamanho[i]);
 
-	printf("Tempo decorrido: %lf\n", tempo);
-
-	FILE* escrita = fopen("comunidades.txt", "w");
+	FILE* escrita = fopen("Datasets/SlashDot/comunidades.txt", "w");
 	for(int i = 0; i < clusters; i++){
 		for (int j = 0; j < n; j++)
 			if (centro[j] == i)
 				fprintf(escrita, "%i ", j);
 		fprintf(escrita, "\n");
 	}
-
+	fclose(escrita);
 
 	return 0;
 }
--- LDG.cpp
+++ LDG.cpp
@@ -16,8 +16,8 @@ int main(int argc, char const *argv[])
 	int a, b;
 	while (fscanf(f, 	"%i %i", &a, &b) != EOF){
 		m[a].push_back(b);
-		//se descomentar a linha de baixo o grafo deixa de ser dirigido
-		//m[b].push_back(a);
+		//se comentar a linha de baixo o grafo passa a ser dirigido
+		m[b].push_back(a);
 	}
 	fclose(f);
 
@@ -25,87 +25,37 @@ int main(int argc, char const *argv[])
 	int clusters = atoi(argv[2]);
 
 	//vetor que vai guardar a que comunidade pertence cada vertice
-	int centro[n];
+	vector <int> centro (n, -1);
 	//vetor que guarda o tamanho de cada cluster
-	int tamanho[clusters];
-	for(int i = 0; i < n; i++){
-		centro[i] = -1;
-		if (i < clusters)
-			tamanho[i] = 0;
-	}
+	vector <int> tamanho (clusters, 0);
 
-	clock_t t = clock();
-
-	//fila usada para fazer a bfs
-	queue <int> fila;
+	//define as comunidades dos primeiros vertices
 	for(int i = 0; i < clusters; i++){
-		fila.push(i);
-		centro[i] = i;
+		centro[rand() % n] = i;
 		tamanho[i] += 1;
 	}
 
 	//capacidade maxima de cada cluster
 	double limite = (double) n / clusters;
-	limite *= 1.01;
-
-	for(int k = 0; k < n; k++){
-		if(centro[k] > -1)
-			continue;
-			//bfs
-
-		fila.push(k);
-		while(fila.size()){
-			int u = fila.front();
-			fila.pop();
-
-			for(int i = 0; i < m[u].size(); i++){
-				//se ja foi visitado vai pro proximo
-				if(centro[m[u][i]] > -1)
-					continue;
-
-				//se nao foi visitado, escolhe uma comunidade e coloca na fila
-				vector <double> coeficiente (clusters, 0);
-
-				//calcula o numero de vizinhos com cada comunidade
-				for (int j = 0; j < m[u].size(); j++){
-					int indice = centro[m[u][j]];
-					if (indice > -1)
-						coeficiente[indice] += 1;
-				}
-
-				//calcula o coeficeinte
-				for(int j = 0; j < clusters; j++)
-					coeficiente[j] *= (1 - (double) tamanho[j]/limite);
-
-				//descobre a comunidade que tem o maior coeficiente
-				int maior = 0;
-				for(int j = 1; j < clusters; j++){
-					if (coeficiente[j] > coeficiente[maior])
-						maior = j;
-					//criterio de desempate e o tamanho da comunidade
-					else if (coeficiente[j] == coeficiente[maior])
-							maior = tamanho[maior] > tamanho[j]? j : maior;
-				}
-
-				//coloca ele na maior comunidade
-				centro[m[u][i]] = maior;
-				tamanho[maior] += 1;
-
-				//adiciona na fila
-				fila.push(m[u][i]);
-			}
-		}
-	}
+	//o tanto que pode variar o tamanho
+	//limite *= 1.1;
 
 
-	//Proximas iteracoes
-	int iteracoes = atoi(argv[3]) - 1;
+	int iteracoes = atoi(argv[3]);
 	for(int k = 0; k < iteracoes; k++){
+
+		//quais vertices ja foram visitados nessa iteracao
 		vector <int> vis (n, 0);
+
+		queue <int> fila;
+		fila.push(rand() % n);
+
 		for (int l = 0; l < n; l++){
+			//se ja foi visitado continua
 			if (vis[l])
 				continue;
 
+			queue <int> fila;
 			fila.push(l);
 
 			//bfs
@@ -113,55 +63,53 @@ int main(int argc, char const *argv[])
 				int u = fila.front();
 				fila.pop();
 
+				if(vis[u])
+					continue;
+
+				//marca como visitado
+				vis[u] = 1;
+				//adiciona os vizinhos na fila
+				for(int i = 0; i < m[u].size(); i++)
+					if(!vis[m[u][i]])
+						fila.push(m[u][i]);
+
+				//se, na primeira iteracao, ja tiver comunidade, pula
+				if(!k && centro[u] > -1)
+					continue;
+
+				vector <double> coeficiente (clusters, 0);
+
 				for(int i = 0; i < m[u].size(); i++){
-					//se ja foi visitado vai pro proximo
-					if(vis[m[u][i]])
-						continue;
-
-					vis[m[u][i]] = 1;
-					//se nao foi visitado, escolhe uma comunidade e coloca na fila
-					vector <double> coeficiente (clusters, 0);
-
-					for (int j = 0; j < m[u].size(); j++){
-						int indice = centro[m[u][j]];
+					int indice = centro[m[u][i]];
+					if (indice > -1)
 						coeficiente[indice] += 1;
-					}
-
-					for(int j = 0; j < clusters; j++)
-						coeficiente[j] *= (1 - (double) tamanho[j]/limite);
-
-					//descobre a comunidade que tem o maior coeficiente
-					int maior = 0;
-					for(int j = 1; j < clusters; j++){
-						if (coeficiente[j] > coeficiente[maior])
-							maior = j;
-						else if (coeficiente[j] == coeficiente[maior])
-								maior = tamanho[maior] > tamanho[j]? j : maior;
-					}
-
-					//tira da comunidade atual
-					tamanho[centro[m[u][i]]] -= 1;
-					//coloca ele na maior comunidade
-					centro[m[u][i]] = maior;
-					tamanho[maior] += 1;
-
-					//adiciona na fila
-					fila.push(m[u][i]);
 				}
+
+				for(int i = 0; i < clusters; i++)
+					coeficiente[i] *= (1.0 - (double) tamanho[i]/limite);
+
+				//descobre qual e o cluster com maior coeficiente
+				int maior = 0;
+				for(int j = 1; j < clusters; j++){
+					if (coeficiente[j] > coeficiente[maior])
+						maior = j;
+					//o criterio de desempate e o tamanho
+					else if (coeficiente[j] == coeficiente[maior])
+						maior = tamanho[maior] > tamanho[j]? j : maior;
+					}
+
+				centro[u] = maior;
+				tamanho[maior] += 1;
 			}
 		}
-	}
 
-	t = clock() - t;
-	double tempo = (double) t / CLOCKS_PER_SEC;
-	printf("Tempo decorrido: %lf\n", tempo);
+		if (k < iteracoes - 1)
+			for(int i = 0; i < clusters; i++)
+				tamanho[i] = 0;
+	}
 
 	for(int i = 0; i < clusters; i++)
 		printf("O tamanho do cluster %i e %i\n", i + 1, tamanho[i]);
-
-	// for(int i = 0; i < n; i++)
-	// 	printf("%i\n", centro[i]);
-
 
 	FILE* escrita = fopen("Datasets/SlashDot/comunidades.txt", "w");
 	for(int i = 0; i < clusters; i++){
