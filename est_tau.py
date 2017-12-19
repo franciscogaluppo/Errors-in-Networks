8498cf8eee87ba12a2b0db7ceebc7ce7ad7fa3dc
settifoglio <froma.galuppo@gmail.com>
2017-12-19 20:23:06 -0200

    Estimativas por regressão

--- est_tau.py
+++ est_tau.py
@@ -23,6 +23,7 @@ print("ATE real: {}".format(ATE))
 # Bitmaps
 z1 = np.zeros(shape=(N))
 z0 = np.zeros(shape=(N))
+uniao = np.zeros(shape=(N))
 
 # Divide os dois grupos
 for i in range(N):
@@ -43,6 +44,10 @@ tam_z0 = int(sum(z0))
 if not tam_z0 or not tam_z1:
 	print("Erro.")
 
+for i in range(N):
+	if z1[i] or z0[i]:
+		uniao[i] = 1
+
 
 # Diferença das médias
 soma_z1 = 0
@@ -58,61 +63,34 @@ ATE_diff = soma_z1/tam_z1 - soma_z0/tam_z0
 print("ATE est: {}".format(ATE_diff))
 
 
-# # Diferença de regressões lineares
+# Diferença de regressões lineares
+tam = int(sum(uniao))
+tau = np.empty(shape=(tam))
+feat = np.empty(shape=(tam, 3))
+y = np.empty(shape=(tam))
+aux = 0
 
-# tau_z1 = np.empty(shape=(tam_z1))
-# tau_z0 = np.empty(shape=(tam_z0))
+# Vetor tau
+for i in range(N):
+	if uniao[i]:
+		soma = 0.0
+		for k in g.neighbors(i):
+			soma += np.float64(zvec[k])
+		tau[aux] = soma/g.degree(i)
+		aux += 1
 
-# feat_z1 = np.empty(shape=(tam_z1, 3))
-# feat_z0 = np.empty(shape=(tam_z0, 3))
+aux = 0
 
-# y_z1 = np.empty(shape=(tam_z1))
-# y_z0 = np.empty(shape=(tam_z0))
+# Vetor das features
+for i in range(N):
+	if uniao[i]:
+		feat[aux][0] = 1
+		feat[aux][1] = zvec[i]
+		feat[aux][2] = tau[aux]
+		y[aux] = yvec[i]
+		aux += 1
 
-# aux_z1 = 0
-# aux_z0 = 0
-
-# # Vetor tau
-# for i in range(N):
-# 	if z1[i]:
-# 		soma = 0.0
-# 		for k in g.neighbors(i):
-# 			soma += np.float64(zvec[k])
-# 		tau_z1[aux_z1] = soma/g.degree(i)
-# 		aux_z1 += 1
-
-# 	elif z0[i]:
-# 		soma = 0.0
-# 		for k in g.neighbors(i):
-# 			soma += np.float64(zvec[k])
-# 		tau_z0[aux_z0] = soma/g.degree(i)
-# 		aux_z0 += 1			
-
-
-# aux_z1 = 0
-# aux_z0 = 0
-
-# # Vetor das features
-# for i in range(N):
-# 	if z1[i]:
-# 		feat_z1[aux_z1][0] = 1
-# 		feat_z1[aux_z1][1] = zvec[i]
-# 		feat_z1[aux_z1][2] = tau_z1[aux_z1]
-# 		y_z1[aux_z1] = yvec[i]
-# 		aux_z1 += 1
-
-# 	elif z0[i]:
-# 		feat_z0[aux_z0][0] = 1
-# 		feat_z0[aux_z0][1] = zvec[i]
-# 		feat_z0[aux_z0][2] = tau_z0[aux_z0]
-# 		y_z0[aux_z0] = yvec[i]
-# 		aux_z0 += 1
-
-# # Regressões
-# lr_z1 = linear_model.LinearRegression().fit(feat_z1, y_z1).coef_
-# lr_z0 = linear_model.LinearRegression().fit(feat_z0, y_z0).coef_
-
-# print(lr_z1[0], lr_z1[1], lr_z1[2], lr_z0[0], lr_z0[1], lr_z0[2])
-
-# ATE_reg = (lr_z1[1] + lr_z1[2]) - (lr_z0[1] + lr_z0[2])
-# print("ATE reg: {}".format(ATE_reg))
+# Regressões
+lr = linear_model.LinearRegression().fit(feat, y).coef_
+ATE_reg = lr[1] + lr[2]
+print("ATE reg: {}".format(ATE_reg))
--- teste.py
+++ teste.py
@@ -1,5 +1,6 @@
 import funcs as f
 import numpy as np
+from sklearn import linear_model
 
 # Setup
 nome = "email-Eu-core"
@@ -17,6 +18,11 @@ N = g.number_of_nodes()
 # Bitmaps
 z1 = np.zeros(shape=(N))
 z0 = np.zeros(shape=(N))
+
+# Bitmaps
+z1 = np.zeros(shape=(N))
+z0 = np.zeros(shape=(N))
+uniao = np.zeros(shape=(N))
 
 # Divide os dois grupos
 for i in range(N):
@@ -37,6 +43,10 @@ tam_z0 = int(sum(z0))
 if not tam_z0 or not tam_z1:
 	print("Erro.")
 
+for i in range(N):
+	if z1[i] or z0[i]:
+		uniao[i] = 1
+
 
 # Diferença das médias
 def diff(z1, z0, yvec, N):
@@ -52,6 +62,39 @@ def diff(z1, z0, yvec, N):
 	return(soma_z1/tam_z1 - soma_z0/tam_z0)
 
 
+# Diferença das regressões
+def reg(uniao, yvec, N):
+	tam = int(sum(uniao))
+	tau = np.empty(shape=(tam))
+	feat = np.empty(shape=(tam, 3))
+	y = np.empty(shape=(tam))
+	aux = 0
+
+	# Vetor tau
+	for i in range(N):
+		if uniao[i]:
+			soma = 0.0
+			for k in g.neighbors(i):
+				soma += np.float64(zvec[k])
+			tau[aux] = soma/g.degree(i)
+			aux += 1
+
+	aux = 0
+
+	# Vetor das features
+	for i in range(N):
+		if uniao[i]:
+			feat[aux][0] = 1
+			feat[aux][1] = zvec[i]
+			feat[aux][2] = tau[aux]
+			y[aux] = yvec[i]
+			aux += 1
+
+	# Regressões
+	lr = linear_model.LinearRegression().fit(feat, y).coef_
+	return(lr[1] + lr[2])
+
+
 X = np.empty(shape=(len(betas), rodadas))
 ATE = np.empty(shape=(len(betas), rodadas))
 
@@ -60,7 +103,8 @@ for i in range(len(betas)):
 	for j in range(rodadas):
 		U = np.random.normal(0, 1, N)
 		yvec = f.simulate(g, model, zvec, beta, ins, U)
-		X[i][j] = diff(z1, z0, yvec, N)
+		#X[i][j] = diff(z1, z0, yvec, N)
+		X[i][j] = reg(uniao, yvec, N)
 		ATE[i][j] = f.real_ATE(g, model, beta, ins, U)
 		print("Beta: {}/{} | Rodada: {}/{}".format(i+1, len(betas), j+1, rodadas))
 
@@ -70,9 +114,9 @@ for i in range(len(betas)):
 
 for i in range(len(betas)):
 	beta = betas[i]
-	arq = open("Comparativos/Modelo_tau(0.5) Diferença de médias binário-email " + str(beta) + ".txt", "w")
+	arq = open("Comparativos/Modelo_tau(0.5) Diferença de Regressões Binário-email " + str(beta) + ".txt", "w")
 	arq.write("# MSE: {}\n# ATE, Diferença de médias\n".format(mse[i]))
 
 	for k in range(rodadas):
 		arq.write("{}, {}\n".format(ATE[i][k], X[i][k]))
-	arq.close()	
+	arq.close()
