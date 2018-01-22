c84bf0f5e81684a5a53d63be4a3faae4c40070f9
settifoglio <froma.galuppo@gmail.com>
2018-01-22 19:11:49 -0200

    Novos grafos

---
+++ trans.py
@@ -0,0 +1,14 @@
+from sys import argv
+from os import system
+import re
+
+arq = open("set.csv", "r")
+out = open(".out", "w")
+
+for i in arq:
+	string = re.split("\s|,", i)
+	out.write("{} {}\n".format(string[0], string[1]))
+arq.close()
+out.close()
+
+system("mv .out set.txt")
